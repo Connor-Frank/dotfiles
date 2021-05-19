a30469fc73c488d09c07f3e4ea948132bcbb02fb
Connor Frank <connorjfrank@gmail.com>
2021-05-19 19:41:44 -0400

    updates

--- arch/.local/src/dwm-6.2/config.h
+++ arch/.local/src/dwm-6.2/config.h
@@ -153,6 +153,7 @@ static Key keys[] = {
            "--profile=low-latency --input-conf=/dev/null --title=webcam $(ls "
            "/dev/video[0,2,4,6,8] | tail -n 1)")},
     {SUPKEY, XK_s, spawn, {.v = sbuild}},
+    {SUPKEY, XK_w, spawn, SHCMD("monero-wallet-gui")},
     /*
      * scratchpads:
      * 0 - terminal
