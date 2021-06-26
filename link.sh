489d0162d248099943f1b28da852acc3638c0133
Connor Frank <connorjfrank@gmail.com>
2021-06-26 13:58:10 -0400

    improved link script

--- link.sh
+++ link.sh
@@ -7,10 +7,12 @@ if [ "$(uname -s)" = "Darwin" ]; then
   ln -s ~/dotfiles/mac/.local/* ~/.local
   ln -s ~/dotfiles/mac/.iTerm ~
 else
-  ln -s ~/dotfiles/general/.config/* ~/.config
-  ln -s ~/dotfiles/general/.vim ~
-  ln -s ~/dotfiles/linux/.zprofile ~/.zprofile
-  ln -s ~/dotfiles/linux/.zshrc ~/.zshrc
-  ln -s ~/dotfiles/linux/.config/* ~/.config
-  ln -s ~/dotfiles/linux/.local/* ~/.local
+  ln -sf ~/dotfiles/general/.config/* ~/.config
+  ln -sf ~/dotfiles/general/.vim ~
+  ln -sf ~/dotfiles/linux/.zprofile ~/.zprofile
+  ln -sf ~/dotfiles/linux/.zshrc ~/.zshrc
+  ln -sf ~/dotfiles/linux/.config/* ~/.config
+  ln -sf ~/dotfiles/linux/.local/bin/* ~/.local/bin
+  ln -sf ~/dotfiles/linux/.local/src/* ~/.local/bin
+  ln -sf ~/dotfiles/linux/.local/wallpapers/* ~/.local/bin
 fi
